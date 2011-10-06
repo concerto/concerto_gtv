aae44e5533cf1ec1ec05157103439d69b71f19f8
Brian Michalski <bmichalski@gmail.com>
2011-10-05 21:15:57 -0700

    Lots of work all over the place.

--- src/com/concerto/concertotv/ConcertoTV.java
+++ src/com/concerto/concertotv/ConcertoTV.java
@@ -2,8 +2,10 @@ package com.concerto.concertotv;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.util.DisplayMetrics;
 
 public class ConcertoTV {
   public static String macAddress(Context context){
@@ -11,13 +13,50 @@ public class ConcertoTV {
     WifiInfo wifiInf = wifiMan.getConnectionInfo();
     return wifiInf.getMacAddress();
   }
-  public static String getURL(){
-    return "";
+  
+  public static String getURL(Context context, DisplayMetrics metrics){
+    String concerto_url;
+    concerto_url = ConcertoTV.getBaseURL(context) + "?mac=" + ConcertoTV.getMac(context);
+    concerto_url += "&width=" + metrics.widthPixels;
+    concerto_url += "&height=" + metrics.heightPixels;
+    
+    return concerto_url;
   }
-  public static String getMac(){
-    return "";
+  
+  public static boolean save(Context context, String url, String mac){
+    SharedPreferences settings = ConcertoTV.getSettings(context);
+    Editor editor = settings.edit();
+    editor.putString("base_url", url);
+    editor.putString("mac_address", mac);
+    editor.putBoolean("first_run", false);
+    return editor.commit();
   }
-  public static String getBaseURL(){
-    return "";
+  
+  public static String getMac(Context context){
+    SharedPreferences settings = ConcertoTV.getSettings(context);
+    String mac =  settings.getString("mac_address", ConcertoTV.macAddress(context));
+    if(mac.length() <= 0){
+      return ConcertoTV.macAddress(context);
+    }
+    return mac;
   }
+  
+  public static String getBaseURL(Context context){
+    SharedPreferences settings = ConcertoTV.getSettings(context);
+    String url = settings.getString("base_url", context.getString(R.string.default_base_url));
+    if(url.length() <= 0){
+      return context.getString(R.string.default_base_url);
+    }
+    return url;
+  }
+  
+  public static boolean firstRun(Context context){
+    SharedPreferences settings = ConcertoTV.getSettings(context);
+    return settings.getBoolean("first_run", true);
+  }
+  
+  public static SharedPreferences getSettings(Context context){
+    return context.getSharedPreferences(context.getString(R.string.pref_file), 0);
+  }
+  
 }
--- src/com/concerto/concertotv/MainActivity.java
+++ src/com/concerto/concertotv/MainActivity.java
@@ -1,12 +1,9 @@
 package com.concerto.concertotv;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 
 public class MainActivity extends Activity {
@@ -22,19 +19,15 @@ public class MainActivity extends Activity {
     DisplayMetrics metrics = new DisplayMetrics();
     getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-    SharedPreferences settings = getSharedPreferences(
-        getString(R.string.pref_file), 0);
-    String base_url = settings.getString("base_url",
-        getString(R.string.default_base_url));
-    String mac_addr = settings.getString("mac_address", ConcertoTV.macAddress(this.getApplicationContext()));
-
-    String concerto_url;
-    concerto_url = base_url + "?mac=" + mac_addr;
-    concerto_url += "&width=" + metrics.widthPixels;
-    concerto_url += "&height=" + metrics.heightPixels;
-
     mWebView = (WebView) findViewById(R.id.webview);
     mWebView.getSettings().setJavaScriptEnabled(true);
-    mWebView.loadUrl(concerto_url);
+    mWebView.loadUrl(ConcertoTV.getURL(getApplicationContext(), metrics));
+  }
+  
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+      finish();
+    }
+    return super.onKeyDown(keyCode, event);
   }
 }
