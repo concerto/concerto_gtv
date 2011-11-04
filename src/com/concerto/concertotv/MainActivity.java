d7afb3cf9c5b21fe7ae4d0d75dc0ffd21d1d8a5a
Brian Michalski <bmichalski@gmail.com>
2011-11-04 00:52:45 -0700

    Add a loading throbber, set the background to black so its not so bright.

--- src/com/concerto/concertotv/MainActivity.java
+++ src/com/concerto/concertotv/MainActivity.java
@@ -1,9 +1,11 @@
 package com.concerto.concertotv;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 public class MainActivity extends Activity {
@@ -19,13 +21,33 @@ public class MainActivity extends Activity {
     DisplayMetrics metrics = new DisplayMetrics();
     getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
+    String server = ConcertoTV.getBaseURL(getApplicationContext());
+    if (server.equals(getString(R.string.default_base_url))) {
+      server = "Concerto TV Server";
+    }
+    final ProgressDialog dialog = ProgressDialog.show(this, "Loading",
+        "Connecting to:\n" + server);
+
     mWebView = (WebView) findViewById(R.id.webview);
+    mWebView.setBackgroundColor(android.graphics.Color.BLACK);
+
     mWebView.getSettings().setJavaScriptEnabled(true);
     mWebView.getSettings().setBuiltInZoomControls(false);
     mWebView.setInitialScale(100);
+
+    mWebView.setWebChromeClient(new WebChromeClient() {
+      @Override
+      public void onProgressChanged(WebView view, int progress) {
+        dialog.setProgress(progress * 10);
+        if (progress == 100) {
+          dialog.dismiss();
+        }
+      }
+    });
     mWebView.loadUrl(ConcertoTV.getURL(getApplicationContext(), metrics));
   }
   
+  @Override
   public boolean onKeyDown(int keyCode, KeyEvent event) {
     if ((keyCode == KeyEvent.KEYCODE_BACK)) {
       finish();
