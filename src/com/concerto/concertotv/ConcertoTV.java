23ec0c18e888e3211d398d78120fe28a84b34a28
Brian Michalski <bmichalski@gmail.com>
2012-04-02 22:51:20 -0700

    Move settings to dialogs.  Add intents to launch the app automatically.  Support gingerbread.
    * Settings were refactored into AlertDialogs accessed via your system's standard settings button.
    * GoogleTV specific flags were removed and the minSDKversion should be compatible with Gingerbread (2.3.3)
    * Intent filters were added so Concerto can be loaded via applications using the concerto:// and http://goto.your-concerto.com/ syntax.
    * Added some documentation to the code too.
    * Bumping to 0.2.0, V3.

--- src/com/concerto/concertotv/ConcertoTV.java
+++ src/com/concerto/concertotv/ConcertoTV.java
@@ -3,75 +3,182 @@ package com.concerto.concertotv;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.net.Uri;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.provider.Settings.Secure;
 import android.util.DisplayMetrics;
 
 public class ConcertoTV {
-  public static String macAddress(Context context){
-    WifiManager wifiMan = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+  /**
+   * Get the mac address, or something that looks like one.
+   * 
+   * @param context
+   * @return String with the device's mac address.
+   */
+  public static String macAddress(Context context) {
+    WifiManager wifiMan = (WifiManager) context
+        .getSystemService(Context.WIFI_SERVICE);
     WifiInfo wifiInf = wifiMan.getConnectionInfo();
     String mac = wifiInf.getMacAddress();
     // If the mac address is null, return something that is workable.
-    if (mac == null){
-      String uniqueId = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID);
+    if (mac == null) {
+      String uniqueId = Secure.getString(context.getContentResolver(),
+          Secure.ANDROID_ID);
       // We hope the last 9 chars are sufficiently unique.
       mac = "001" + uniqueId.substring(0, 9);
     }
     return mac;
   }
-  
-  public static String getURL(Context context, DisplayMetrics metrics){
+
+  /**
+   * Build a URL with the screen information and default server and mac address.
+   * 
+   * @param context
+   * @param metrics
+   * @return URL to load.
+   */
+  public static String getURL(Context context, DisplayMetrics metrics) {
     String concerto_url;
-    concerto_url = ConcertoTV.getBaseURL(context) + "?mac=" + ConcertoTV.getMac(context);
+    concerto_url = ConcertoTV.getBaseURL(context, false) + "?mac="
+        + ConcertoTV.getMac(context);
     concerto_url += "&width=" + metrics.widthPixels;
     concerto_url += "&height=" + metrics.heightPixels;
-    
+
     return concerto_url;
   }
-  
-  public static boolean save(Context context, String url, String mac){
+
+  /**
+   * Build a URL with the screen information and custom server and mac address.
+   * 
+   * @param context
+   * @param baseURL
+   * @param mac
+   * @param metrics
+   * @return URL to load.
+   */
+  public static String getCustomURL(Context context, String baseURL,
+      String mac, DisplayMetrics metrics) {
+    String concerto_url;
+    concerto_url = baseURL + "?mac=" + mac;
+    concerto_url += "&width=" + metrics.widthPixels;
+    concerto_url += "&height=" + metrics.heightPixels;
+
+    return concerto_url;
+  }
+
+  /**
+   * Save the concerto server URL.
+   * 
+   * @param context
+   * @param url
+   * @return Save successful.
+   */
+  public static boolean saveUrl(Context context, String url) {
     SharedPreferences settings = ConcertoTV.getSettings(context);
     Editor editor = settings.edit();
     editor.putString("base_url", url);
-    editor.putString("mac_address", mac);
-    editor.putBoolean("first_run", false);
     return editor.commit();
   }
-  
-  public static boolean stopFirstRun(Context context){
+
+  /**
+   * Save the mac address.
+   * 
+   * @param context
+   * @param mac
+   * @return Save successful.
+   */
+  public static boolean saveMac(Context context, String mac) {
     SharedPreferences settings = ConcertoTV.getSettings(context);
     Editor editor = settings.edit();
-    editor.putBoolean("first_run", false);
+    editor.putString("mac_address", mac);
     return editor.commit();
   }
-  
-  public static String getMac(Context context){
+
+  /**
+   * Get the save mac address or fall back to the device's current info.
+   * 
+   * @param context
+   * @return Mac address.
+   */
+  public static String getMac(Context context) {
     SharedPreferences settings = ConcertoTV.getSettings(context);
-    String mac =  settings.getString("mac_address", ConcertoTV.macAddress(context));
-    if(mac.length() <= 0){
+    String mac = settings.getString("mac_address",
+        ConcertoTV.macAddress(context));
+    if (mac.length() <= 0) {
       return ConcertoTV.macAddress(context);
     }
     return mac;
   }
-  
-  public static String getBaseURL(Context context){
+
+  /**
+   * Get the current server URL to connect to.
+   * 
+   * @param context
+   * @param hide_default
+   *          Don't return the URL if it's the default.
+   * @return server url.
+   */
+  public static String getBaseURL(Context context, Boolean hide_default) {
     SharedPreferences settings = ConcertoTV.getSettings(context);
-    String url = settings.getString("base_url", context.getString(R.string.default_base_url));
-    if(url.length() <= 0){
-      return context.getString(R.string.default_base_url);
+    String url = settings.getString("base_url",
+        context.getString(R.string.default_base_url));
+    if (url.length() <= 0) {
+      url = context.getString(R.string.default_base_url);
     }
-    return url;
+    if (url == context.getString(R.string.default_base_url) && hide_default) {
+      return "";
+    } else {
+      return url;
+    }
   }
-  
-  public static boolean firstRun(Context context){
-    SharedPreferences settings = ConcertoTV.getSettings(context);
-    return settings.getBoolean("first_run", true);
+
+  public static SharedPreferences getSettings(Context context) {
+    return context.getSharedPreferences(context.getString(R.string.pref_file),
+        0);
   }
-  
-  public static SharedPreferences getSettings(Context context){
-    return context.getSharedPreferences(context.getString(R.string.pref_file), 0);
+
+  /**
+   * Extract the concerto server from an URL loading the application.
+   * 
+   * @param data
+   * @return Concerto server URL.
+   */
+  public static String parseBaseURL(Uri data) {
+    Uri.Builder builder = data.buildUpon();
+    if (data.getScheme().equals("concerto")) {
+      builder.scheme("http");
+    } else {
+      if (data.getPathSegments().size() > 0) {
+        // Copy the server to replace goto.your-concerto.com
+        String host = data.getPathSegments().get(0);
+        builder.authority(host);
+        // and remove it from the path.
+        builder.path(data.getPath().replace(host, ""));
+      } else {
+        // There is no server specified so clear everything out.
+        builder.authority("");
+        builder.path("");
+      }
+    }
+    // Clear out the query string. We don't care about it here.
+    builder.query("");
+    Uri composed = builder.build();
+    if (composed.getPath().length() > 0) {
+      return composed.toString();
+    } else {
+      return null;
+    }
   }
-  
+
+  /**
+   * Extract the mac address from a URI loading the application.
+   * 
+   * @param data
+   * @return Mac address.
+   */
+  public static String parseMac(Uri data) {
+    String mac = data.getQueryParameter("mac");
+    return mac;
+  }
 }
--- src/com/concerto/concertotv/MainActivity.java
+++ src/com/concerto/concertotv/MainActivity.java
@@ -1,23 +1,39 @@
 package com.concerto.concertotv;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.KeyEvent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+  EditText serverInput;
+  EditText identifierInput;
   WebView mWebView;
+  AlertDialog serverDialog;
+  AlertDialog identifierDialog;
 
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    
+
     mWebView = (WebView) findViewById(R.id.webview);
     mWebView.setBackgroundColor(android.graphics.Color.BLACK);
 
@@ -25,42 +41,188 @@ public class MainActivity extends Activity {
     mWebView.getSettings().setBuiltInZoomControls(false);
     mWebView.setInitialScale(100);
 
-    String server = ConcertoTV.getBaseURL(getApplicationContext());
-    if (server.equals(getString(R.string.default_base_url))) {
+    String server = ConcertoTV.getBaseURL(getApplicationContext(), true);
+    if (server.length() == 0) {
       server = "Concerto TV Server";
     }
     final ProgressDialog dialog = ProgressDialog.show(this, "Loading",
         "Connecting to:\n" + server);
-    
+
+    // Handle redirects and other URL changes inside the app.
+    mWebView.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+      }
+    });
+
+    // Track the first loading process in a dialog window.
     mWebView.setWebChromeClient(new WebChromeClient() {
       @Override
       public void onProgressChanged(WebView view, int progress) {
         dialog.setProgress(progress * 10);
-        if (progress == 100) {
+        if (progress >= 100) {
           dialog.dismiss();
         }
       }
     });
+
+    final Context context = this;
+    setupServerMenu(context);
+    setupIdentifierMenu(context);
   }
-  
-  public void onStart(){
+
+  /**
+   * Setup the Change Server menu.
+   * 
+   * @param context
+   */
+  private void setupServerMenu(Context context) {
+    LayoutInflater li = LayoutInflater.from(context);
+
+    View promptsView = li.inflate(R.layout.server_prompt, null);
+    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+    alertDialogBuilder.setView(promptsView);
+
+    serverInput = (EditText) promptsView.findViewById(R.id.editServer);
+
+    alertDialogBuilder.setCancelable(false).setTitle(R.string.server_prompt)
+        .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int id) {
+            String newServer = serverInput.getText().toString();
+            if (!newServer.equals(ConcertoTV.getBaseURL(
+                getApplicationContext(), true))) {
+              ConcertoTV.saveUrl(getApplicationContext(), newServer);
+              loadConcerto();
+            }
+          }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int id) {
+            dialog.cancel();
+          }
+        });
+
+    serverDialog = alertDialogBuilder.create();
+  }
+
+  /**
+   * Setup the Change Identifier menu.
+   * 
+   * @param context
+   */
+  private void setupIdentifierMenu(Context context) {
+    LayoutInflater li = LayoutInflater.from(context);
+
+    View promptsView = li.inflate(R.layout.identifier_prompt, null);
+    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+    alertDialogBuilder.setView(promptsView);
+    identifierInput = (EditText) promptsView.findViewById(R.id.editIdentifier);
+
+    alertDialogBuilder.setCancelable(false)
+        .setTitle(R.string.identifier_prompt)
+        .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int id) {
+            String newMac = identifierInput.getText().toString();
+            if (!newMac.equals(ConcertoTV.getMac(getApplicationContext()))) {
+              ConcertoTV.saveMac(getApplicationContext(), newMac);
+              loadConcerto();
+            }
+          }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int id) {
+            dialog.cancel();
+          }
+        });
+
+    identifierDialog = alertDialogBuilder.create();
+  }
+
+  @Override
+  public void onStart() {
     super.onStart();
+    Intent intent = getIntent();
+    Uri data = intent.getData();
+    if (data != null) {
+      String server = ConcertoTV.parseBaseURL(data);
+      if (server == null) {
+        server = ConcertoTV.getBaseURL(getApplicationContext(), false);
+      }
+      String mac = ConcertoTV.parseMac(data);
+      if (mac == null) {
+        mac = ConcertoTV.getMac(getApplicationContext());
+      }
+
+      loadCustomConcerto(server, mac);
+    } else {
+      loadConcerto();
+    }
+  }
+
+  /**
+   * Load a non-default concerto server and mac address
+   * 
+   * @param server
+   * @param mac
+   */
+  public void loadCustomConcerto(String server, String mac) {
     DisplayMetrics metrics = new DisplayMetrics();
     getWindowManager().getDefaultDisplay().getMetrics(metrics);
-    
+    Log.v("connect",
+        ConcertoTV.getCustomURL(getApplicationContext(), server, mac, metrics));
+    mWebView.loadUrl(ConcertoTV.getCustomURL(getApplicationContext(), server,
+        mac, metrics));
+  }
+
+  /**
+   * Load the default concerto server.
+   */
+  public void loadConcerto() {
+    DisplayMetrics metrics = new DisplayMetrics();
+    getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
     mWebView.loadUrl(ConcertoTV.getURL(getApplicationContext(), metrics));
   }
-  
-  public void onStop(){
+
+  @Override
+  public void onStop() {
+    // Head to about:blank to stop hitting the concerto server
     mWebView.loadUrl("about:blank");
     super.onStop();
   }
-  
+
   @Override
-  public boolean onKeyDown(int keyCode, KeyEvent event) {
-    if ((keyCode == KeyEvent.KEYCODE_BACK)) {
-      finish();
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.server:
+        serverDialog();
+        return true;
+      case R.id.identifier:
+        identifierDialog();
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
     }
-    return super.onKeyDown(keyCode, event);
+  }
+
+  private void serverDialog() {
+    serverInput.setText(ConcertoTV.getBaseURL(getApplicationContext(), true));
+    serverDialog.show();
+  }
+
+  private void identifierDialog() {
+    identifierInput.setText(ConcertoTV.getMac(getApplicationContext()));
+    identifierDialog.show();
   }
 }
